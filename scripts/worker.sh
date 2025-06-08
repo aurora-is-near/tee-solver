f7dbb35219594b3d8e8461982316c0339af3d983
Robert Yan <mstr.raphael@gmail.com>
2025-06-09 01:05:07 +0800

    chore: update solver codehash

--- scripts/worker.sh
+++ scripts/worker.sh
@@ -1,7 +1,7 @@
 export NEAR_ENV=testnet
 export SOLVER_REGISTRY_CONTRACT=solver-registry-dev.testnet
 export SOLVER_GOV_ACCOUNT=solver-gov.testnet
-export WORKER_CODEHASH=b08c929607a344db24191b59c388abdca2bc9721d8eab7fb740b19de03a7d690
+export WORKER_CODEHASH=73056c3446210e747b2ed3d5dfad5f842b31e4a9155c0feaeeb45a3f515816fc
 
 # approve worker codehash
 near call $SOLVER_REGISTRY_CONTRACT approve_codehash '{"codehash":"'$WORKER_CODEHASH'"}' --accountId $SOLVER_GOV_ACCOUNT
