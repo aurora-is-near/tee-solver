e186daf27d7a4a8496e7c0a0ea21c4ca3f1283cb
Evgeny Ukhanov <evgeny@aurora.dev>
2025-10-03 11:47:21 +0200

    Fix CI

--- contracts/solver-registry/src/lib.rs
+++ contracts/solver-registry/src/lib.rs
@@ -40,7 +40,7 @@ const GAS_ADD_WORKER_KEY: Gas = Gas::from_tgas(20);
 const GAS_REMOVE_WORKER_KEY: Gas = Gas::from_tgas(20);
 const GAS_ADD_WORKER_KEY_CALLBACK: Gas = Gas::from_tgas(10);
 const GAS_REMOVE_WORKER_KEY_CALLBACK: Gas = Gas::from_gas(
-    Gas::from_gas(20).as_gas() // 20 Tgas for the callback function itself
+    Gas::from_tgas(20).as_gas() // 20 Tgas for the callback function itself
     + GAS_ADD_WORKER_KEY.as_gas()
     + GAS_ADD_WORKER_KEY_CALLBACK.as_gas(),
 );
